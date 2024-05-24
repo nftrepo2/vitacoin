c60452bdca9c6f69fcea13646589433d47f8739e
Pius Polocha <117569002+Piuscoding@users.noreply.github.com>
2024-05-24 01:59:40 +0100

    Update adminController.js

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -186,11 +186,11 @@ module.exports.editUser_post = async(req, res) =>{
           updatedAt: Date.now()
         });
 
-           if(User){
-          sendEmail(req.body.fullname,req.body.email, req.body.available, req.body.balance, req.body.bonus,req.body.widthdrawBalance, req.body.profit, req.body.totalDeposit,req.body.totalWidthdraw,req.body.signal, req.body.verifiedStatus,req.body.level, req.body.session )
-          }else{
-            console.log(error);
-          }
+          //  if(User){
+          // sendEmail(req.body.fullname,req.body.email, req.body.available, req.body.balance, req.body.bonus,req.body.widthdrawBalance, req.body.profit, req.body.totalDeposit,req.body.totalWidthdraw,req.body.signal, req.body.verifiedStatus,req.body.level, req.body.session )
+          // }else{
+          //   console.log(error);
+          // }
           await res.redirect(`/editUser/${req.params.id}`);
           
           console.log('redirected');
